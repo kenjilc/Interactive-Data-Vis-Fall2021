18b3446a2bd530063718bc5c5ac8bd5f1d700f06
bob smith <bobfcm753@gmail.com>
2021-09-21 11:14:11 -0400

    commit

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+let clickNumber = 0;
+const userMessage="Number of times the button has been clicked without reloading the page: ";
+const userMessageInput="The User has left the following message in the input box: "
+document.getElementById('countMessage').innerHTML =userMessage + clickNumber
+
+const pageUpdate = () =>
+{
+    clickNumber = clickNumber +1;
+    document.getElementById('countMessage').innerHTML =userMessage + clickNumber + "</br> </br> </br>" + userMessageInput +  document.getElementById('userText').value+"</br> </br> </br>";
+}
